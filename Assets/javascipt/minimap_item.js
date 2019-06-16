cf0e08762c4038665195a3356608989ec23413b8
unknown <shyunju7@naver.com>
2019-06-17 00:13:52 +0900

    add minimap

---
+++ Assets/javascipt/minimap_item.js
@@ -0,0 +1,66 @@
+﻿#pragma strict
+
+private var call_minimap : boolean;
+var cameraManager : CameraManager;
+private var item_count : int; // 가지고 있는 미니맵 아이템 수
+private var sw : int;
+private var sh : int;
+var labelStyle : GUIStyle;
+
+function Start () {
+	//call_minimap = false;
+	item_count = 0;
+}
+
+function Update () {
+	// miniMap 호출키를 누르면
+	if(Input.GetKey("1")){
+		getMapItem();
+	}
+}
+
+
+// trigger에 들어가면
+function OnTriggerEnter(other:Collider){
+	
+	if(other.gameObject.tag == "drone"){ // if tag name is drone...
+		item_count ++; // 미니맵 아이템 추가
+		call_minimap = true;
+	}
+}
+
+// trigger를 나가면
+function OnTriggerExit(other:Collider){
+	if(other.gameObject.tag == "drone"){ // destroy drone...
+		yield WaitForSeconds(0.2);
+		//call_minimap = false;
+		Destroy(other.gameObject);
+	}
+}
+
+function getMapItem(){
+	
+	if(item_count >= 1){ // minimap_item >= 1
+		cameraManager.miniCamOn(); // call minimap mode
+		
+		//4초 후에 사라짐
+		yield WaitForSeconds(4.0);
+		cameraManager.mainCamOn();
+		item_count --; // 아이템 사용했음으로 -1
+	}
+	
+	else{
+		GUI.Label(Rect(sw/6,sh/3, sw*2/3,sh/3),"No ITEM!", labelStyle);
+	}
+
+}
+
+function OnGUI(){
+	if(Input.GetKey("1")){ 
+		if(item_count == 0){
+			sw = Screen.width;
+			sh = Screen.height;
+			GUI.Label(Rect(sw/6,sh/3, sw*2/3,sh/3),"No ITEM!", labelStyle);
+		}
+	}
+}
